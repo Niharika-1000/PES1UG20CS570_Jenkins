decf514f8c91bfb38a404fe2b5fea787f9cbed7b
Niharika-1000 <kanchamniharika@gmail.com>
2023-02-19 18:05:03 +0530

    PES1UG20CS570-Niharika Kancham

---
+++ main/pipeline.cpp
@@ -0,0 +1,9 @@
+
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "I have successfully built and run pipeline\n";
+  cout << "PES1UG20CS570-Niharika Kancham";
+  return 0;
+}
